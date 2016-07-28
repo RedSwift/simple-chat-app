81517a024f858bc833fa3a62bd1a3f87245fa307
Dominic <jxdlam@gmail.com>
2016-07-28 16:32:06 +0800

    migrated to components

--- public/main.component.js
+++ public/main.component.js
@@ -1,7 +1,17 @@
 angular.module('chatApp')
   .component('mainChat', {
     templateUrl: 'main.template.html',
-    controller: function () {
-      
+    controller: function ($scope, socket) {
+      var ctrl = this
+      this.msgs = []
+      this.sendMsg = function () {
+        event.preventDefault()
+        socket.emit('send msg', this.msgText)
+        this.msgText = ''
+      }
+      socket.on('get msg', function (data) {
+        ctrl.msgs.push(data)
+        $scope.$digest()
+      })
     }
   })
